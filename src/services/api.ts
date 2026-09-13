b0fb8dff3234c13e5252b19bde80091ed5bd1362
YOBRA-coder <56080569+YOBRA-coder@users.noreply.github.com>
2026-09-13 12:32:30 +0300

    Update API URL for token refresh endpoint

--- src/services/api.ts
+++ src/services/api.ts
@@ -40,7 +40,7 @@ api.interceptors.response.use(
         if (!refreshToken) throw new Error('No refresh token');
 
         const { data } = await axios.post(
-          `${'http://localhost:3000/api/v1'}/auth/refresh`,
+          `${'https://pesa.zonmrkt.com/api/v1'}/auth/refresh`,
           { refreshToken }
         );
         const newToken = data.data.accessToken;
