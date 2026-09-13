047fa26eb1592745e641c31d51df2b4580512795
YOBRA-coder <56080569+YOBRA-coder@users.noreply.github.com>
2026-09-13 13:03:28 +0300

    Add __filename and __dirname constants

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path';
 import { fileURLToPath } from 'url';
 import { VitePWA } from 'vite-plugin-pwa';
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 export default defineConfig({
   plugins: [
     react(),
